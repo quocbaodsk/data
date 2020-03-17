e2b17e41503f37a55431257393f590eb9f8ea34c
quocbaodsk <56862552+quocbaodsk@users.noreply.github.com>
2020-03-17 19:22:40 +0700

    Update auto.php

--- auto.php
+++ auto.php
@@ -1,1 +1,11 @@
-error
+<?php
+function ping($host, $port, $timeout) { 
+  $tB = microtime(true); 
+  $fP = fSockOpen($host, $port, $errno, $errstr, $timeout); 
+  if (!$fP) { return "down"; } 
+  $tA = microtime(true); 
+  return round((($tA - $tB) * 1000), 0)." ms"; 
+}
+
+//Echoing it will display the ping if the host is up, if not it'll say "down".
+echo ping($_GET['d'], 80, 10);  
